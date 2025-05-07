53373d50efa572fb9f9fb3b7b7897c4f322061e1
Jumin325 <pjm05144@gmail.com>
2025-05-07 15:07:38 +0900

    데이터 추가본

--- (new)dump-capstone.sql
+++ (new)dump-capstone.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: capstone
 -- ------------------------------------------------------
--- Server version	8.4.3
+-- Server version	8.0.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -33,7 +33,7 @@ CREATE TABLE `book` (
   PRIMARY KEY (`book_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `book_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,1,'임헌찬 외 2인','북두출판사','9791166754548','전자공학과',2024),(2,2,'한학근','문운당','9791156922483','전자공학과',2016),(3,3,'한학근','동일출판사','9788938111876','전자공학과',2018),(4,4,'김경희 외 1인','한빛아카데미','9791156645306','정보통신과',2021),(5,5,'김수원 외 7인','홍릉과학출판사','9788945001658','정보통신과',2017),(6,6,'이종원','한빛아카데미','9791156641599','정보통신과',2017),(7,7,'검정연구회','동일출판사','9788938116871','전기과',2024),(8,8,'손혜영','인피니티북스','9788992649919','전기과',2016),(9,9,'천인국','생능출판사','9788970506678','전기과',2023);
+INSERT INTO `book` VALUES (1,1,'임헌찬 외 2인','북두출판사','9791166754548','전자공학과',2024),(2,2,'한학근','문운당','9791156922483','전자공학과',2016),(3,3,'한학근','동일출판사','9788938111876','전자공학과',2018),(4,4,'김경희 외 1인','한빛아카데미','9791156645306','정보통신과',2021),(5,5,'김수원 외 7인','홍릉과학출판사','9788945001658','정보통신과',2017),(6,6,'이종원','한빛아카데미','9791156641599','정보통신과',2017),(7,7,'검정연구회','동일출판사','9788938116871','전기과',2024),(8,8,'손혜영','인피니티북스','9788992649919','전기과',2016),(9,9,'천인국','생능출판사','9788970506678','전기과',2023),(10,10,'하정우','북두출판사','9791166754487','전자공학과',2024),(11,11,'오종오, 김승겸','북두출판사','9791159066245','전자공학과',2015),(12,12,'임석구, 홍경호','한빛아카데미','9791156645689','전자공학과',2022),(13,13,'김종현','생능출판','9791192932941','정보통신과',2024),(14,14,'우재남','한빛아카데미','9791156645702','정보통신과',2022),(15,15,'이해선','북두출판사','9791166753244','정보통신과',2023),(16,16,'이충식 외 4명','동일출판사','9788938106773','전기과',2010),(17,17,'검정연구회','동일출판사','9788938116888','전기과',2025),(18,18,'김세동 외 3명','동일출판사','9788938117045','전기과',2025);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +64,7 @@ CREATE TABLE `order_items` (
   KEY `product_id` (`product_id`),
   CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
   CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +73,7 @@ CREATE TABLE `order_items` (
 
 LOCK TABLES `order_items` WRITE;
 /*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
+INSERT INTO `order_items` VALUES (1,1,1,1,16000.00);
 /*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,8 +91,9 @@ CREATE TABLE `orders` (
   `total_amount` decimal(10,2) DEFAULT NULL,
   `discount_amount` decimal(10,2) DEFAULT '0.00',
   `phone` varchar(13) DEFAULT NULL,
+  `session_id` varchar(255) NOT NULL,
   PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,6 +102,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'2025-05-07 04:24:38','준비',NULL,0.00,NULL,'v9fl8l5YeFmZ9gNmlkl9Q-XjDOLPgHvq');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +124,7 @@ CREATE TABLE `product` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +133,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'전기기초실험',16000.00,'/images/1.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(2,'이론과 함께 하는 전자회로 실험',26000.00,'/images/2.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(3,'NCS기반 전기회로 실험',22000.00,'/images/3.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(4,'컴퓨터 활용과 실습 2019',25000.00,'/images/4.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(5,'전자회로',42000.00,'/images/5.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(6,'페도라리눅스 시스템&네트워크',29000.00,'/images/6.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(7,'2025 회로이론',19800.00,'/images/7.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(8,'LapVIEW의 정석 기본편',28000.00,'/images/8.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(9,'쉽게 풀어쓴 C언어 Express개정 4판',32200.00,'/images/9.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38');
+INSERT INTO `product` VALUES (1,'전기기초실험',16000.00,'/images/1.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(2,'이론과 함께 하는 전자회로 실험',26000.00,'/images/2.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(3,'NCS기반 전기회로 실험',22000.00,'/images/3.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(4,'컴퓨터 활용과 실습 2019',25000.00,'/images/4.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(5,'전자회로',42000.00,'/images/5.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(6,'페도라리눅스 시스템&네트워크',29000.00,'/images/6.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(7,'2025 회로이론',19800.00,'/images/7.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(8,'LapVIEW의 정석 기본편',28000.00,'/images/8.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(9,'쉽게 풀어쓴 C언어 Express개정 4판',32200.00,'/images/9.jpg','책',10,1,'2025-05-03 12:50:38','2025-05-03 12:50:38'),(10,'회로이론기초',15000.00,'/images/10.jpeg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:47:29'),(11,'실무와 예제로 배우는 PADS VX.O',29000.00,'/images/11.jpg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:28:49'),(12,'디지털 논리회로 이론, 실습 시뮬레이션 개정 4판',32000.00,'/images/12.jpeg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:48:16'),(13,'컴퓨터구조론',31000.00,'/images/13.jpg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:28:49'),(14,'IT CookBook',26000.00,'/images/14.jpeg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:47:52'),(15,'제5판 통신시스템의 기초',26000.00,'/images/15.jpeg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:48:23'),(16,'최신전기 AutoCAD',20000.00,'/images/16.jpg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:28:49'),(17,'2025 전기기기(전기기사시리즈3)',22000.00,'/images/17.jpg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:28:49'),(18,'자가용 전기설비설계',39000.00,'/images/18.jpg','책',10,1,'2025-05-07 05:28:49','2025-05-07 05:28:49');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,4 +202,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-04 22:29:29
+-- Dump completed on 2025-05-07 14:49:17
